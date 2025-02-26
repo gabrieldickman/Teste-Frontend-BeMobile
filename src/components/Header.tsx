5a1185adc185629e1d7957b5a2f2745035aca7ed
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-02-25 21:59:17 -0400

    feat: criado header da pagina

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import styles from "../modules/Header.module.css";
+
+interface HeaderProps {
+  logo: string;
+  altText?: string;
+}
+
+function Header({logo, altText = "BeTalent Logo"}: HeaderProps) {
+  return (
+    <div className={styles.header}>
+      <img src={logo} alt={altText} className={styles.logo}/>
+    </div>
+  )
+}
+
+export default Header
